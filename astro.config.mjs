8343d61fb57d09e3eb055ff52719ff10e2cfee64
Jewelina95 <shaoyue95szd@163.com>
2026-08-31 18:18:38 +0100

    Configure worldeology.com custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'https://jewelina95.github.io',
+  site: process.env.PUBLIC_SITE_URL || 'https://worldeology.com',
   output: 'static',
   build: {
     assets: '_assets'
